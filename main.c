4446e8690edd45826d7a6c54c392a27787e2a6ec
Ioannis Tsagkaropoulos <giannis.tsagkaropoulos@gmail.com>
2026-09-03 13:40:55 +0200

    feat: implement type builtin

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+const int NUM_COMMANDS = 3;
+const char *VALID_COMMANDS[3] = {"exit", "echo", "type"};
+
+void handle_type(char *buf)
+{
+
+  for (int i = 0; i < NUM_COMMANDS; i++)
+  {
+    if (strcmp(buf, VALID_COMMANDS[i]) == 0)
+    {
+      printf("%s is a shell builtin\n", buf);
+      return;
+    }
+  }
+  printf("%s: not found\n", buf);
+}
 
 int main(int argc, char *argv[])
 {
@@ -13,10 +31,15 @@ int main(int argc, char *argv[])
     printf("$ ");
     fgets(user_input, sizeof(user_input), stdin);
     user_input[strcspn(user_input, "\n")] = '\0';
+
     if (strncmp(user_input, "exit", 4) == 0)
       break;
+
     else if (strncmp(user_input, "echo ", 5) == 0)
       printf("%s\n", user_input + 5);
+
+    else if (strncmp(user_input, "type ", 5) == 0)
+      handle_type(user_input + 5);
 
     printf("%s: command not found\n", user_input);
   }
