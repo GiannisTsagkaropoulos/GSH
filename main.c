d05653587a269e954b4cb880fa3dccd27ee902f4
Ioannis Tsagkaropoulos <giannis.tsagkaropoulos@gmail.com>
2026-09-03 13:30:08 +0200

    feat: implement echo builtin

--- main.c
+++ main.c
@@ -2,17 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   setbuf(stdout, NULL);
-  char user_input[1024]; 
+  char user_input[1024];
 
-  while (1) {
+  while (1)
+  {
     printf("---- Welcome to GSH :D ----\n");
     printf("$ ");
     fgets(user_input, sizeof(user_input), stdin);
-    user_input[strcspn(user_input, "\n")] = '\0'; 
+    user_input[strcspn(user_input, "\n")] = '\0';
     if (strncmp(user_input, "exit", 4) == 0)
       break;
+    else if (strncmp(user_input, "echo ", 5) == 0)
+      printf("%s\n", user_input + 5);
 
     printf("%s: command not found\n", user_input);
   }
